3b6198c4cba3015166d43bd1debc5cf54e529cb2
Lucas Nascimento <17522772+lucabelezal@users.noreply.github.com>
2026-04-11 12:16:32 -0300

    chore(test): tipa global como any no setupTests.ts para evitar erro TS7053 e remove @ts-expect-error desnecessário

---
+++ src/setupTests.ts
@@ -0,0 +1,18 @@
+
+// Garante jsdom em ambientes CI onde document/window podem não estar definidos
+import { JSDOM } from 'jsdom';
+if (typeof window === 'undefined' || typeof document === 'undefined') {
+	const dom = new JSDOM('<!doctype html><html><body></body></html>');
+	// @ts-expect-error jsdom: window não existe no Node
+	global.window = dom.window;
+	global.document = dom.window.document;
+	// Copia propriedades úteis do window para global
+	Object.getOwnPropertyNames(dom.window).forEach((prop) => {
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		const g = global as any;
+		if (typeof g[prop] === 'undefined') {
+			g[prop] = dom.window[prop];
+		}
+	});
+}
+
